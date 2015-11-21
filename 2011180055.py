6696e94bb49f8145b3c6325e1e46c1972cab464c
saeunpark <longlong92@naver.com>
2015-11-21 15:10:25 +0900

    몬스터 공격 발사체

--- 2011180055.py
+++ 2011180055.py
@@ -251,7 +251,29 @@ class Monster_Pink:
     def detecting(a, self):
         pass
 
+class Bird_Arrow:
+    image=None
+    PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+    RUN_SPEED_KMPH = 40.0 # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        if Bird_Arrow.image==None:
+            self.image = load_image('bird_arrow.png')
+    def update(self,frame_time):
+        distance = Bird_Arrow.RUN_SPEED_PPS * frame_time
+        #print("a update")
+        self.x-=distance
 
+        self.draw()
+    def get_hitbox(self):
+         return self.x - 10, self.y +10, self.x + 10, self.y-10
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_hitbox())
 def main():
 
     open_canvas()
